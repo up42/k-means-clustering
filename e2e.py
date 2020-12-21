40fc6ca53763f25936551e092850e21f0aa03f71
Rodrigo Almeida <rodrigo.almeida@up42.com>
2020-12-21 15:44:56 +0100

    Add e2e library (#20)

--- e2e.py
+++ e2e.py
@@ -3,53 +3,32 @@ End-to-end test: Fetches data, creates output, stores it in /tmp and checks if o
 is valid.
 """
 from pathlib import Path
-import os
 
 import geojson
 
-from blockutils.logging import get_logger
+from blockutils.e2e import E2ETest
 
-logger = get_logger(__name__)
+# Disable unused params for assert
+# pylint: disable=unused-argument
+def asserts(input_dir: Path, output_dir: Path, quicklook_dir: Path, logger):
+    # Print out bbox of one tile
+    geojson_path = output_dir / "data.json"
+
+    with open(str(geojson_path)) as f:
+        feature_collection = geojson.load(f)
+
+    logger.info(feature_collection.features[0].bbox)
+
+    output = output_dir / feature_collection.features[0].properties["up42.data_path"]
+
+    logger.info(output)
+
+    assert output.exists()
+
 
 if __name__ == "__main__":
-    TESTNAME = "e2e_k-means_clustering"
-    TEST_DIR = Path("/tmp") / TESTNAME
-    TEST_DIR.mkdir(parents=True, exist_ok=True)
-    INPUT_DIR = TEST_DIR / "input"
-    INPUT_DIR.mkdir(parents=True, exist_ok=True)
-    FILES_TO_DELETE = Path(TEST_DIR / "output").glob("*")
-    for file_path in FILES_TO_DELETE:
-        file_path.unlink()
-
-    # Download file from gsutil
-    os.system(
-        "gsutil -m cp -r gs://floss-blocks-e2e-testing/e2e_kmeans_clustering/* %s"
-        % INPUT_DIR
-    )
-
-    RUN_CMD = (
-        """docker run -v %s:/tmp \
-                 -e 'UP42_TASK_PARAMETERS={"n_clusters":8, "n_iterations": 10, "n_sieve_pixels": 64}' \
-                  -it kmeans-clustering"""
-        % TEST_DIR
-    )
-
-    os.system(RUN_CMD)
-
-    # Print out bbox of one tile
-    GEOJSON_PATH = TEST_DIR / "output" / "data.json"
-
-    with open(str(GEOJSON_PATH)) as f:
-        FEATURE_COLLECTION = geojson.load(f)
-
-    logger.info(FEATURE_COLLECTION.features[0].bbox)
-
-    OUTPUT = (
-        TEST_DIR
-        / "output"
-        / Path(FEATURE_COLLECTION.features[0].properties["up42.data_path"])
-    )
-
-    logger.info(OUTPUT)
-
-    assert OUTPUT.exists()
+    e2e = E2ETest("kmeans-clustering")
+    e2e.add_parameters({"n_clusters": 8, "n_iterations": 10, "n_sieve_pixels": 64})
+    e2e.add_gs_bucket("gs://floss-blocks-e2e-testing/e2e_kmeans_clustering/*")
+    e2e.asserts = asserts
+    e2e.run()
